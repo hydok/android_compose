f66013603bd0345f15b61ff87fbabce67ea01c12
hydok <hydok@naver.com>
2022-11-10 13:03:33 +0900

    order screen add

---
+++ app/src/main/java/io/hydok/andoridcompose/ui/SummaryScreen.kt
@@ -0,0 +1,19 @@
+package io.hydok.andoridcompose.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import io.hydok.andoridcompose.data.OrderUiState
+
+
+@Composable
+fun OrderSummaryScreen(
+    orderUiState: OrderUiState,
+    onCancelButtonClicked: () -> Unit,
+    onSendButtonClicked: (String, String) -> Unit,
+    modifier: Modifier = Modifier
+) {
+
+
+
+
+}

9d30dce15c637ce6a22c0509799acf2041be9157
hydok <hydok@naver.com>
2022-11-10 13:46:28 +0900

    summary screen

--- app/src/main/java/io/hydok/andoridcompose/ui/SummaryScreen.kt
+++ app/src/main/java/io/hydok/andoridcompose/ui/SummaryScreen.kt
@@ -1,8 +1,21 @@
 package io.hydok.andoridcompose.ui
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.Divider
+import androidx.compose.material.OutlinedButton
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import io.hydok.andoridcompose.R
 import io.hydok.andoridcompose.data.OrderUiState
+import io.hydok.andoridcompose.ui.components.FormattedPriceLabel
 
 
 @Composable
@@ -12,8 +25,71 @@ fun OrderSummaryScreen(
     onSendButtonClicked: (String, String) -> Unit,
     modifier: Modifier = Modifier
 ) {
+    val resources = LocalContext.current.resources
+
+    //갯수 메세지 설정
+    val numberOfCupcakes = resources.getQuantityString(
+        R.plurals.cupcakes,
+        orderUiState.quantity,
+        orderUiState.quantity
+    )
+
+    //공유 메세지 설정
+    val orderSummary = stringResource(
+        id = R.string.order_details, numberOfCupcakes,
+        orderUiState.flavor,
+        orderUiState.date,
+        orderUiState.quantity
+    )
 
 
+    val newOrder = stringResource(R.string.new_cupcake_order)
 
+    //보여줄 데이터 세팅
+    val dataItems = listOf(
+        Pair(stringResource(R.string.quantity), numberOfCupcakes),
+        Pair(stringResource(R.string.flavor), orderUiState.flavor),
+        Pair(stringResource(R.string.pickup_date), orderUiState.date)
+    )
+
+    Column (
+        modifier = modifier.padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+
+        dataItems.forEach { item ->
+            Text(item.first.uppercase())
+            Text(text = item.second, fontWeight = FontWeight.Bold)
+            Divider(thickness = 1.dp)
+        }
+        
+        Spacer(modifier = Modifier.height(8.dp))
+        FormattedPriceLabel(
+            subtotal = orderUiState.price,
+            modifier = Modifier.align(Alignment.End)
+        )
+        Button(
+            modifier = Modifier.fillMaxWidth(),
+            onClick = { onSendButtonClicked(newOrder, orderSummary) }
+        ) {
+            Text(stringResource(R.string.send))
+        }
+        OutlinedButton(
+            modifier = Modifier.fillMaxWidth(),
+            onClick = onCancelButtonClicked
+        ) {
+            Text(stringResource(R.string.cancel))
+        }
+    }
+}
+
+@Preview
+@Composable
+fun OrderSummaryPreview(){
+    OrderSummaryScreen(
+        orderUiState = OrderUiState(0, "Test", "Test", "$300.00"),
+        onSendButtonClicked = { subject: String, summary: String -> },
+        onCancelButtonClicked = {}
+    )
 
 }
